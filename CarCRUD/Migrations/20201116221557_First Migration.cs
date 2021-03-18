7cccd1db2acee45f1304781cf1e4b0df97002492
tsta19 <tsta19@student.aau.dk>
2021-03-18 20:39:33 +0100

    Added the Visual Studio Project

---
+++ CarCRUD/Migrations/20201116221557_First Migration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarCRUD.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CarParts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Details = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarParts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarParts");
+        }
+    }
+}
---
+++ CarCRUD/Repositories/PartRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Threading.Tasks;
+using CarCRUD.Models;
+using System.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using CarCRUD.DbContexts;
+
+namespace CarCRUD.Repositories
+{
+    public class PartRepository
+    {
+        private CarPartsDbContext _context;
+        public PartRepository(CarPartsDbContext context) 
+        {
+            _context = context;
+        }
+        public CarPart Get(int id)
+        {
+            var carPart = _context.CarParts.Where(a => a.Id == id).First();
+            return carPart;
+        }
+        public List<CarPart> List(int Limit = 5)
+        {
+            return _context.CarParts.ToList();
+        }
+        public void Create(CarPart carPart)
+        {
+            _context.Add(carPart);
+            _context.SaveChanges();
+        }
+        public void Delete(int id)
+        {
+            _context.Remove(_context.CarParts.Single(a => a.Id == id));
+            _context.SaveChanges();
+        }
+        public void Update(int id, CarPart carPart)
+        {
+            var carPartOld = _context.CarParts.First(a => a.Id == id);
+            carPartOld.Name = carPart.Name;
+            carPartOld.Price = carPart.Price;
+            carPartOld.Details = carPart.Details;
+            _context.SaveChanges();
+        }
+        public CarPart FillPart(SqlDataReader result)
+        {
+            var CarPart = new CarPart();
+            CarPart.Id = (int)result.GetValue(0);
+            CarPart.Name = (string)result.GetValue(1);
+            CarPart.Price = (double)result.GetValue(2);
+            CarPart.Details = (string)result.GetValue(3);
+            return CarPart;
+        }
+    }
+}
