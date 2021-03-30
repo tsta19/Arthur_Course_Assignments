2ad6da54725352eb78986509afbaf353229d4201
Jakub <jaksad4@gmail.com>
2021-03-30 13:32:24 +0200

    Changed the Add page

--- CarCRUD/CarCRUD/wwwroot/scripts/AddPart.js
+++ CarCRUD/CarCRUD/wwwroot/scripts/AddPart.js
@@ -1,1 +1,32 @@
-﻿
+﻿function getCarPartFromForm() {
+    var name = $('#name').val();
+    var price = $('#price').val();
+    var details = $('#details').val();
+    return {
+        name: name,
+        price: price,
+        details: details
+    }
+}
+
+function createCarPart() {
+    var response = null;
+    console.log(getCarPartFromForm());
+    console.log(JSON.stringify(getCarPartFromForm()));
+    $.ajax({
+        url: "https://localhost:5001/api/CarParts/create",
+        contentType: "application/json",
+        method: "POST",
+        async: false,
+        data: JSON.stringify(getCarPartFromForm()),
+        success: function (data) {
+            response = data;
+        },
+        error: function (er) {
+            alert("error")
+            console.log(er)
+
+        }
+    });
+    return response;
+}
